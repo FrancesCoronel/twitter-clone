2f4d406b73a260ca67095b55d1c90565001cf306
Kaito Hara <kaito.hara@me.com>
2015-06-17 12:59:18 -0400

    added Swig templating

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 // requiring all dependencies
 var express = require('express');
 var morgan = require('morgan');
+var swig = require('swig');
+
+//turning off Swig's caching
+swig.setDefaults({ cache: false });
 
 // creating server using express
 var app = express();
+
+//render html
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
 
 // listening to server at port 3000
 var server = app.listen(3000);
@@ -12,5 +21,6 @@ app.use(morgan('dev'));
 
 // routing
 app.use('/', function(request, response) {
-	response.send("hello");
+	var people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+	response.render( 'index', {title: 'Hall of Fame', people: people} );
 });
